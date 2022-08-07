4ce623de6b6871188432920cea7e821c66a3721c
Dispersioned <dispersioned@gmail.com>
2022-08-07 17:16:15 +0300

    fix: linters conflicts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next';
+import Head from 'next/head';
 import Image from 'next/image';
 
-import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => (
@@ -13,18 +13,13 @@ const Home: NextPage = () => (
     </Head>
 
     <main className={styles.main}>
+      {' '}
       <h1 className={styles.title}>
-        Welcome to
-        {' '}
-        <a href="https://nextjs.org">Next.js!</a>
+        Welcome to <a href="https://nextjs.org">Next.js!</a>
       </h1>
-
       <p className={styles.description}>
-        Get started by editing
-        {' '}
-        <code className={styles.code}>pages/index.tsx</code>
+        Get started by editing <code className={styles.code}>pages/index.tsx</code>
       </p>
-
       <div className={styles.grid}>
         <a href="https://nextjs.org/docs" className={styles.card}>
           <h2>Documentation &rarr;</h2>
@@ -57,8 +52,7 @@ const Home: NextPage = () => (
         target="_blank"
         rel="noopener noreferrer"
       >
-        Powered by
-        {' '}
+        Powered by{' '}
         <span className={styles.logo}>
           <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
         </span>
