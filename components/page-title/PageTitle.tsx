d18e55f25d63be01b58a3b4b7a12c26cd1eaf3de
Dispersioned <dispersioned@gmail.com>
2022-08-14 12:59:18 +0300

    feat: page title adaptivity

--- components/page-title/PageTitle.tsx
+++ components/page-title/PageTitle.tsx
@@ -1,14 +1,34 @@
-import { Typography } from '@mui/material';
+import { Typography, styled } from '@mui/material';
 import React from 'react';
 
 type PageTitleProps = {
   text: string;
 };
 
+const Text = styled(Typography)`
+  font-size: 3.5rem;
+  margin: 1.5rem 0;
+  @media (max-width: 1199px) {
+    font-size: 3rem;
+    margin: 1.2rem 0;
+  }
+  @media (max-width: 991px) {
+    font-size: 2.5rem;
+    margin: 1rem 0;
+  }
+  @media (max-width: 767px) {
+    font-size: 2.2rem;
+  }
+  @media (max-width: 575px) {
+    font-size: 2rem;
+    margin: 0.8rem 0;
+  }
+`;
+
 export const PageTitle = ({ text }: PageTitleProps) => {
   return (
-    <Typography component="h1" variant="h2" style={{ margin: '1.5rem 0' }}>
+    <Text component="h1" variant="h2">
       {text}
-    </Typography>
+    </Text>
   );
 };
